07e3dc20c5266f792eb720ec11d8e491ebfe5ad9
Mark Timbol <Mark@Mark.local>
2016-05-02 17:31:19 +0400

    Design: Add parallax

--- resources/views/public/menu.blade.php
+++ resources/views/public/menu.blade.php
@@ -2,41 +2,43 @@
 
 @section('content')
     <div class="Page">
-        <div class="Page__header--bg">
-            <div class="Page__header">  
-                <div class="SearchContainer">
-                    <div class="form-group">
-                        <label>What are you looking for?</label>
-                        <input type="text" name="search" class="form-control input-lg" placeholder="Sandwich, Burger, Hot Dogs" />
-                    </div>
-                </div>   
-            </div>
-        </div>
-
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="Page__title">Our Menus</h2>
-                </div>
-
-                <div class="Menus">
-                    @foreach( range(1, 12) as $index )
-                    <div class="col-md-4 col-xs-12">
-                        <div class="Card">
-                            <a href="#" class="Card__link">
-                                <div class="Card__image">
-                                    <img src="/images/menus/bacon.jpg" alt="" title="" class="img-responsive" />
-                                </div>
-                                <div class="Card__content">
-                                    <h3 class="Card__title">NEW! Triple Bacon Sampler</h3>
-                                </div>
-                            </a>
+        <div class="Parallax">
+            <div class="Page__header--bg Parallax__item">
+                <div class="Page__header">  
+                    <div class="SearchContainer">
+                        <div class="form-group">
+                            <label>What are you looking for?</label>
+                            <input type="text" name="search" class="form-control input-lg" placeholder="Sandwich, Burger, Hot Dogs" />
                         </div>
-                    </div>
-                    @endforeach
+                    </div>   
                 </div>
             </div>
-        </div>  
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="Page__title">Our Menus</h2>
+                    </div>
+
+                    <div class="Menus">
+                        @foreach( range(1, 12) as $index )
+                        <div class="col-md-4 col-xs-12">
+                            <div class="Card">
+                                <a href="#" class="Card__link">
+                                    <div class="Card__image">
+                                        <img src="/images/menus/bacon.jpg" alt="" title="" class="img-responsive" />
+                                    </div>
+                                    <div class="Card__content">
+                                        <h3 class="Card__title">NEW! Al's Italian Beef</h3>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>  
+        </div>
     </div>
     
 @endsection
